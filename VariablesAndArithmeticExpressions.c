95a0eb64ec49f9347ecd6bf5589194603a5898be
Timothy Siwula <tcsiwula@dons.usfca.edu>
2014-01-28 19:09:58 -0800

    Demo C code

---
+++ VariablesAndArithmeticExpressions.c
@@ -0,0 +1,33 @@
+//
+//  VariablesAndArithmeticExpressions.c
+//  C
+//
+//  Created by Tim on 12/31/13.
+//  Copyright (c) 2013 Tim Siwula. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+/* print Fahrenheit-Celsius table
+    for fahr = 0, 20, ..., 300 */
+
+int happy()
+{
+    int fahr, celsius;
+    int lower, upper, step;
+    
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+    
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
