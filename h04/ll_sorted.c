32cacd946d80b63f9039b3b44ea578d80b57b4a6
Tim Siwula <timsiwula@icloud.com>
2014-12-29 00:30:26 -0600

    files from class

---
+++ h04/ll_sorted.c
@@ -0,0 +1,138 @@
+/* Author: Tim Siwula
+*
+*  File:     ll_sorted
+ *
+ * Purpose:  Implement a sorted linked list with ops Insert 
+ *           and Print.
+ * 
+ * Input:    Single character lower case letters to indicate operations, 
+ *           followed by arguments needed by operations.
+ *
+ * Output:   Results of operations.
+ *
+ * Compile:  gcc -g -Wall -o lls ll_sorted.c
+ * Run:      ./lls
+ *
+ * Notes:
+ *    1.  Repeated values are allowed in the list
+ *    2.  Program assumes an int will be entered when prompted
+ *        for one.
+ *    3.  The insert function is missing some code ...
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list_node_s {
+   int    data;
+   struct list_node_s* next_p;
+};
+
+struct list_node_s* Insert(struct list_node_s* head_p, int val);
+void Print(struct list_node_s* head_p);
+char Get_command(void);
+int  Get_value(void);
+
+/*-----------------------------------------------------------------*/
+int main(void) {
+   char command;
+   int  value;
+   struct list_node_s* head_p = NULL;  
+      /* start with empty list */
+
+   command = Get_command();
+   while (command != 'q' && command != 'Q') {
+      switch (command) {
+         case 'i': 
+         case 'I': 
+            value = Get_value();
+            head_p = Insert(head_p, value);
+            break;
+         case 'p':
+         case 'P':
+            Print(head_p);
+            break;
+         default:
+            printf("There is no %c command\n", command);
+            printf("Please try again\n");
+      }
+      command = Get_command();
+   }
+
+   return 0;
+}  /* main */
+
+/*-----------------------------------------------------------------*/
+struct list_node_s* Insert(struct list_node_s* head_p, int val) {
+   struct list_node_s* curr_p = head_p;
+   struct list_node_s* pred_p = NULL;
+   struct list_node_s* temp_p;
+
+   // go through the list and compare until inserting value finds the right place to insert value
+   while (curr_p != NULL) {
+      if (curr_p->data >= val) 
+         break;
+      pred_p = curr_p;           // 
+      curr_p = curr_p->next_p;
+   } 
+
+ // insert in the front of the list or insert into an empty list
+if (pred_p == NULL)
+{
+   temp_p = malloc(sizeof(struct list_node_s));    // sets temp_p size 
+   temp_p->data = val;
+   temp_p->next_p = curr_p;
+   return temp_p;
+}
+   // normal case: insert nums in the middle of list or at the tail of list
+   temp_p = malloc(sizeof(struct list_node_s));    // sets temp_p size 
+   temp_p->data = val;        // sets inserted value to temp_p's data
+   temp_p->next_p = curr_p;
+   pred_p->next_p = temp_p;   
+   return head_p;
+
+}  /* Insert */
+
+
+/*-----------------------------------------------------------------
+ * Function:   Print
+ * Purpose:    print list on a single line of stdout
+ * Input arg:  head_p
+ */
+void Print(struct list_node_s* head_p) {
+   struct list_node_s* curr_p = head_p;
+
+   printf("list = ");
+   while (curr_p != NULL) {
+      printf("%d ", curr_p->data);
+      curr_p = curr_p->next_p;
+   }  
+   printf("\n");
+}  /* Print */
+
+/*-----------------------------------------------------------------
+ * Function:      Get_command
+ * Purpose:       Get a single character command from stdin
+ * Return value:  The first non-whitespace character from stdin
+ */
+char Get_command(void) {
+   char c;
+
+   printf("Please enter a command (i, p, q):  ");
+   /* Put the space before the %c so scanf will skip white space */
+   scanf(" %c", &c);
+   return c;
+}  /* Get_command */
+
+/*-----------------------------------------------------------------
+ * Function:   Get_value
+ * Purpose:    Get an int from stdin
+ * Return val: The next int in stdin
+ * Note:       Behavior unpredictable if an int isn't entered
+ */
+int  Get_value(void) {
+   int val;
+
+   printf("Please enter a value:  ");
+   scanf("%d", &val);
+   return val;
+}  /* Get_value */
---
+++ h11/hw11-tcsiwula.c
@@ -0,0 +1,139 @@
+/****************************************************************
+ *	Grade: 0/4
+ *	Commets: wrong implementation.
+ *
+ *
+ *
+ *
+ ***************************************************************/
+/* File:
+ *    hw_11.c
+ *      Author: Tim Siwula
+ * Purpose:
+ *    Generate random numbers with function that isn't threadsafe.
+ *
+ * Compile:
+ *    gcc -g -Wall -o hw_11 hw_11.c -lpthread
+ * Usage:
+ *    ./hw_11 <thread_count> <number of random numbers per thread>
+ *
+ * Input:
+ *    None
+ * Output:
+ *    Random numbers from each thread
+ *
+ * Warning:
+ *    Tim Siwula is godsend and made this program thread safe.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+//#include <semaphore.h>
+
+
+#define MR_MULTIPLIER 279470273 
+#define MR_INCREMENT 0
+#define MR_MODULUS 4294967291U
+
+int thread_count;
+int n;
+
+
+void Usage(char* prog_name);
+
+unsigned My_random(unsigned seed);
+
+void *Thread_work(void* rank);  /* Thread function */
+
+/*--------------------------------------------------------------------*/
+int main(int argc, char* argv[]) {
+   long        thread;
+   pthread_t* thread_handles; 
+
+   if (argc != 3) Usage(argv[0]);
+   thread_count = strtol(argv[1], NULL, 10);
+   n = strtol(argv[2], NULL, 10);
+
+   thread_handles = malloc(thread_count*sizeof(pthread_t));
+
+   printf("Enter text\n");
+   for (thread = 0; thread < thread_count; thread++)
+      pthread_create(&thread_handles[thread], NULL,
+          Thread_work, (void*) thread);
+
+   for (thread = 0; thread < thread_count; thread++) 
+      pthread_join(thread_handles[thread], NULL);
+
+   free(thread_handles);
+   return 0;
+}  /* main */
+
+
+/*--------------------------------------------------------------------
+ * Function:    Usage
+ * Purpose:     Print command line for function and terminate
+ * In arg:      prog_name
+ */
+void Usage(char* prog_name) {
+
+   fprintf(stderr, "usage: %s <thread count> <number of random vals per thread>\n", 
+         prog_name);
+   exit(0);
+}  /* Usage */
+
+
+/*-------------------------------------------------------------------
+ * Function:    Thread_work
+ * Purpose:     Each thread calls a function and prints a message
+ * In arg:      rank
+ * Global vars: thread_count (in) 
+ * Return val:  Ignored
+ */
+void *Thread_work(void* rank) 
+{
+    long my_rank = (long) rank;
+    int i;
+    int seed = 0;
+
+ 
+   My_random(seed);  /* "Seed" Random number generator */
+  
+   for (i = 0; i < n; i++) 
+   {
+
+    printf("Th %ld > %u\n", my_rank, My_random(0));
+    seed = my_rank+1;
+}
+   return NULL;
+}  /* Thread_work */
+
+
+/*-------------------------------------------------------------------
+ * Function:    My_random
+ * Purpose:     Generate random numbers
+ * In arg:      seed  (ignored if = 0)
+ * Ret val:     A "random" unsigned int
+ *
+ * Note:        The first time the function is called, the random
+ *              number should be "seeded" with a nonzero argument,
+ *              and the return value ignored.  Subsequent calls 
+ *              should have a zero argument and the return values
+ *              will form a "pseudo-random" sequence.
+ */
+unsigned My_random(unsigned seed) 
+{
+    //unsigned int seed;
+    unsigned long storage;
+   
+   //seed
+   //if (seed != 0) random_number = seed;
+   
+   //compute
+   storage = seed*MR_MULTIPLIER; 
+   seed = storage % MR_MODULUS;
+   
+
+   return seed;
+}  /* My_random */
